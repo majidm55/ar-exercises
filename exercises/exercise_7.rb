8cf300dee40207507d83ced134512d92d8e774f6
Majid <mohammadmajid55@gmail.com>
2019-05-14 21:19:17 +0000

    finished ex7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter a store name"
+  input = gets.chomp
+
+@new_Store = Store.create(name: "#{input}")
+
+if @new_Store.errors.any?
+  @new_Store.errors.each do |attribute, message|
+    puts "Attribute: #{attribute} #{message}"
+  end
+end
